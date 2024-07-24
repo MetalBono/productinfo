3690c924a7f6753a142852428306dab7689b5961
DevBono <gggs.kim@gmail.com>
2024-07-25 00:04:57 +0900

    project initialization commit

---
+++ src/main/kotlin/com/metalbono/service/productinfo_api/ProductinfoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.metalbono.service.productinfo_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductinfoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductinfoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/metalbono/service/productinfo_api/ProductinfoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.metalbono.service.productinfo_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductinfoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
